e9ba5f84faf28a7b95a0f89603ad87003e07ddde
Henry Moore <hmoore@basecampcodingacademy.org>
2018-06-22 11:54:08 -0500

    now using gas type names to select gas type

--- gas_pump.py
+++ gas_pump.py
@@ -2,47 +2,41 @@ from datetime import datetime
 
 
 def get_payment():
-    print('How would you like to pay today?')
+    print('-----------------------------------------------------------')
     payment = input(
-        '\t1 Prepay\n\t2 Pay After\n\tQ Quit\n>>> ').upper().strip()
+        '\n\t1 Prepay\n\t2 Pay After\n\tQ Quit\n>>> ').upper().strip()
     while not (payment == '1' or payment == '2' or payment == 'Q'):
-        print('invalid')
+        print('Invalid Choice... Please Retype Choice')
+        print()
         payment = input('>>> ').upper().strip()
 
     return payment
 
 
 def get_gastype():
-    choices = ['A', 'B', 'C']
-    print('What kind of gas would you like today?')
-    gas_type = input(
-        '\tA Regular\n\tB Premium\n\tC Diesel\n>>> ').upper().strip()
+    choices = 'REGULAR PLUS PREMIUM'.split()
+    print('-----------------------------------------------------------')
+    print('(Regular)', '(Plus)', '(Premium)')
+    gas_type = input('\nWhat Type Of Gas Would You Like? ').upper().strip()
     while not (gas_type in choices):
         print('invalid')
         gas_type = input('>>> ').upper().strip()
-
-    if gas_type == 'A':
-        return 'Regular'
-    elif gas_type == 'B':
-        return 'Premium'
-    elif gas_type == 'C':
-        return 'Diesel'
-
-    return 'Regular'
+    return gas_type
 
 
 def get_gasprice(gas_type):
-    if gas_type == 'Regular':
+    if gas_type == 'REGULAR':
         return 2.50
-    elif gas_type == 'Premium':
+    elif gas_type == 'PLUS':
         return 2.80
-    if gas_type == 'Diesel':
+    elif gas_type == 'PREMIUM':
         return 3.01
 
 
 def main():
-    print('welcome!')
-
+    print('\nWelcome to Gas-Mart!!!\n')
+    name = input('What is your name? ')
+    print('\nHello', name)
     payment = get_payment()
     if payment == 'Q':
         print('thank you come again')
