4d5599e2b3b65eb5fcfa51ef6553b688f2140cd2
Henry Moore <hmoore@basecampcodingacademy.org>
2018-06-22 13:37:54 -0500

    first working version

--- gas_pump.py
+++ gas_pump.py
@@ -10,6 +10,10 @@ def get_payment():
         print()
         payment = input('>>> ').upper().strip()
 
+    if payment == '1':
+        return 'PREPAY'
+    elif payment == '2':
+        return 'PAY AFTER'
     return payment
 
 
@@ -19,18 +23,51 @@ def get_gastype():
     print('(Regular)', '(Plus)', '(Premium)')
     gas_type = input('\nWhat Type Of Gas Would You Like? ').upper().strip()
     while not (gas_type in choices):
-        print('invalid')
+        print('Invalid Choice... Please Retype Choice')
         gas_type = input('>>> ').upper().strip()
+
+    print(gas_type, 'SELECTED')
     return gas_type
 
 
 def get_gasprice(gas_type):
     if gas_type == 'REGULAR':
-        return 2.50
+        price = 2.50
     elif gas_type == 'PLUS':
-        return 2.80
+        price = 2.80
     elif gas_type == 'PREMIUM':
-        return 3.01
+        price = 3.01
+    else:
+        price = 0
+
+    print('${:.2f} per gal'.format(price))
+    return price
+
+
+def get_gallons():
+    print('-----------------------------------------------------------')
+    gallons = input('\nHow Many gallons Are You Buying? ').strip()
+    while not (gallons.isdigit()):
+        print('Invalid Choice... Please Retype Choice')
+        gallons = input('>>> ')
+    return int(gallons)
+
+
+def get_dollars():
+    print('-----------------------------------------------------------')
+    dollars = input('\nHow Much Money are you Spending? ').strip()
+    while not (dollars.isdigit()):
+        print('Invalid Choice... Please Retype Choice')
+        dollars = input('>>> ')
+    return int(dollars)
+
+
+def get_gallons_prepay(price, money):
+    return money / price
+
+
+def get_money_pay_after(price, gallons):
+    return price * gallons
 
 
 def main():
@@ -39,15 +76,25 @@ def main():
     print('\nHello', name)
     payment = get_payment()
     if payment == 'Q':
-        print('thank you come again')
+        print('Thank you come again!!')
         exit()
 
     gas_type = get_gastype()
     price = get_gasprice(gas_type)
 
-    print('payment', payment)
-    print('type', gas_type)
-    print('price', price)
+    if payment == 'PAY AFTER':
+        gallons = get_gallons()
+        money = get_money_pay_after(price, gallons)
+    elif payment == 'PREPAY':
+        money = get_dollars()
+        gallons = get_gallons_prepay(price, money)
+
+    print('Payment Method', payment)
+    print('-----------------------------------------------------------')
+    print('Fuel Type', gas_type)
+    print('\t${:.2f} per gal'.format(price))
+    print('Gallons', round(gallons, 3))
+    print('TOTAL:', '${:.2f}'.format(money))
 
 
 if __name__ == '__main__':
